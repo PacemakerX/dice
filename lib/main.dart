ad53addd87085ce5b1c184bba586eb510733c2fc
Sparsh Soni <sparsh.officialwork@gmail.com>
2025-12-13 18:30:15 +0530

    added a app bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:dice/gradient_container.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: GradientContainer()
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dice'),
+          centerTitle: true,
+          bottomOpacity: 0.5,
+        ),
+
+        body: GradientContainer(),
       ),
-      ),
+    ),
   );
 }
